17cc6d05c355a0c017d10e40518700108dbf23cd
BLXNK-333 <moto-j@outlook.com>
2024-07-04 01:40:48 +0400

    Add solve

---
+++ solutions/py/binary-tree-postorder-traversal.py
@@ -0,0 +1,47 @@
+"""
+Given the root of a binary tree, return the postorder traversal of its nodes'
+values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+
+Example 2:
+Input: root = []
+Output: []
+
+Example 3:
+Input: root = [1]
+Output: [1]
+
+Constraints:
+The number of the nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+Follow up: Recursive solution is trivial, could you do it iteratively?
+"""
+from typing import Optional, List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+
+        def dfs(node):
+            if node is None:
+                return
+            if node.left:
+                dfs(node.left)
+            if node.right:
+                dfs(node.right)
+            result.append(node.val)
+
+        dfs(root)
+        return result
