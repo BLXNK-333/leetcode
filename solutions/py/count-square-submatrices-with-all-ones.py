5e9a415f0d4ae441ad4ad01500241e2448f7d328
BLXNK-333 <moto-j@outlook.com>
2025-08-21 20:27:47 +0400

    Add solve

---
+++ solutions/py/count-square-submatrices-with-all-ones.py
@@ -0,0 +1,58 @@
+"""
+Given a m * n matrix of ones and zeros, return how many square submatrices have
+all ones.
+
+Example 1:
+Input: matrix =
+[
+  [0,1,1,1],
+  [1,1,1,1],
+  [0,1,1,1]
+]
+Output: 15
+Explanation:
+There are 10 squares of side 1.
+There are 4 squares of side 2.
+There is  1 square of side 3.
+Total number of squares = 10 + 4 + 1 = 15.
+
+Example 2:
+Input: matrix =
+[
+  [1,0,1],
+  [1,1,0],
+  [1,1,0]
+]
+Output: 7
+Explanation:
+There are 6 squares of side 1.
+There is 1 square of side 2.
+Total number of squares = 6 + 1 = 7.
+
+Constraints:
+1 <= arr.length <= 300
+1 <= arr[0].length <= 300
+0 <= arr[i][j] <= 1
+"""
+
+from typing import List
+
+
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        n, m = len(matrix), len(matrix[0])
+        mtrx = [[0] * (m + 1) for _ in range(n + 1)]
+        cnt = 0
+
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j]:
+                    x, y = i + 1, j + 1
+                    mtrx[x][y] = 1 + min(mtrx[i][y], mtrx[i][j], mtrx[x][j])
+                    cnt += mtrx[x][y]
+        return cnt
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.countSquares(matrix=[[0, 1, 1, 1], [1, 1, 1, 1], [0, 1, 1, 1]]))
